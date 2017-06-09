8ab29fbfa00340ccd376058821680bbea504029e
Philipp <github-commit@phil-schneider.de>
2017-06-09 12:17:01 +0200

    PartnerList - currency formated PartnerDetail new Query used schema.graphql new imported README.md updated

--- src/partnerDetailMyClaimsListEntry.js
+++ src/partnerDetailMyClaimsListEntry.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import {createFragmentContainer, graphql} from "react-relay";
 
+import currencyFormatter from 'currency-formatter';
+
 const PartnerDetailMyClaimsListEntry = createFragmentContainer(
     ({data}) => (
         <tr>
             <td>{data.claimsNumber}</td>
             <td>{data.description}</td>
-            <td>{data.claimsSum}</td>
+            <td>{currencyFormatter.format(data.claimsSum, { code: 'CHF' })}</td>
             <td>{data.claimsDate}</td>
             <td>{data.state}</td>
         </tr>
